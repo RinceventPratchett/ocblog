1646f43ac6271663d3554814116bbd52dbe5daae
RinceventPratchett <voydu69@gmail.com>
2019-12-20 14:54:13 +0100

    commit initial branche wip

---
+++ model/frontend.php
@@ -0,0 +1,44 @@
+<?php
+
+
+function getPosts()
+{
+    $db = dbConnect();
+    $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM chapter ORDER BY creation_date DESC LIMIT 0, 2');
+
+    return $req;
+}
+
+function getPost($postId)
+{
+    $db = dbConnect();
+    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM chapter WHERE id = ?');
+    $req->execute(array($postId));
+    $post = $req->fetch();
+
+    return $post;
+}
+
+function getComments($postId)
+{
+    $db = dbConnect();
+    $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comment WHERE id_chapter = ? ORDER BY comment_date DESC');
+    $comments->execute(array($postId));
+
+    return $comments;
+}
+
+function postComment($postId, $author, $comment)
+{
+    $db = dbConnect();
+    $comments = $db->prepare('INSERT INTO comment(id_chapter, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+    $affectedLines = $comments->execute(array($postId, $author, $comment));
+
+    return $affectedLines;
+}
+
+function dbConnect()
+{
+        $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'C4m1lle69!');
+        return $db;
+}
