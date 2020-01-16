2b5589a8019b00fe1dc88c80922fd3e061eee0fd
RinceventPratchett <voydu69@gmail.com>
2020-01-16 17:24:58 +0100

    modif btn rejoindre le chap sur index reroutage sur view admin du btn rejoindre chap adminAllCom.

--- view/backend/adminAllComment.php
+++ view/backend/adminAllComment.php
@@ -25,7 +25,7 @@
                 <form action="index.php?action=moderateComment&amp;id=<?= $comment['id'] ?>&postId=<?= $comment['id_chapter'] ?>" onsubmit="return confirm('Voulez vous supprimer le commentaire ?')" method="POST">
                     <input class="btn btn-danger" type="submit" value="supprimer commentaire" />
                 </form>
-                <a href="index.php?action=showPost&amp;id=<?= $comment['id_chapter'] ?>" class="btn btn-info">rejoindre le chapitre</a><br />
+                <a href="index.php?action=adminView&id=<?= $comment['id_chapter'] ?>" class="btn btn-info">rejoindre le chapitre</a><br />
             </div>
         </div>
     </article>
